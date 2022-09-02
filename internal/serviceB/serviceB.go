9df7b9341ff17541876d007c440848953d95de36
Robin <57940220+RobinHAEVG@users.noreply.github.com>
2022-09-02 11:46:37 +0200

    Update serviceB.go

--- internal/serviceB/serviceB.go
+++ internal/serviceB/serviceB.go
@@ -31,7 +31,11 @@ func (sb *ServiceB) Run(ctx context.Context, wg *sync.WaitGroup, logger *logrus.
 	go func() {
 		<-ctx.Done()
 
-		if err := srv.Shutdown(ctx); err != nil && !errors.Is(err, context.Canceled) {
+		// give the server 10 seconds to shut down
+		ctx2, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+		defer cancel()
+		
+		if err := srv.Shutdown(ctx2); err != nil && !errors.Is(err, context.Canceled) {
 			fmt.Printf("failed to shut down Service B server: %s\n", err.Error())
 		}
 	}()
